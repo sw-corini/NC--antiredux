4da0c25ef58299b7abf568f0ae171f87024e2c65
sw <genie_s2@naver.com>
2019-02-12 14:53:18 +0900

    Consuming the Store

---
+++ src/Components/App/AppContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import AppPresenter from "./AppPresenter";
+import Store from "../../store";
+
+class AppContainer extends Component {
+    state = {
+        message: "HEllo"
+    };
+
+    componentDidMount = () => {
+        setTimeout(() => {
+            this.setState({
+                message: "bye"
+            });
+        }, 2000);
+    };
+    render() {
+        return (
+            <Store.Provider value={this.state}>
+                <AppPresenter />
+            </Store.Provider>
+        );
+    }
+}
+
+export default AppContainer;
