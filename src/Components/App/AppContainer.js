fe66fe172d414d264b2a3317673f344397c55fbe
sw <genie_s2@naver.com>
2019-02-13 10:13:14 +0900

    #5 Rendering the Notifications

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,17 +3,25 @@ import AppPresenter from "./AppPresenter";
 import Store from "../../store";
 
 class AppContainer extends Component {
-    state = {
-        message: "HEllo"
-    };
+    constructor(props) {
+        super(props);
+        this._changeMessage = () => {
+            if (this.state.message === "Hello") {
+                this.setState({
+                    message: "Bye bye"
+                });
+            } else {
+                this.setState({
+                    message: "Hello"
+                });
+            }
+        };
+        this.state = {
+            message: "Hello",
+            changeMessage: this._changeMessage
+        };
+    }
 
-    componentDidMount = () => {
-        setTimeout(() => {
-            this.setState({
-                message: "bye"
-            });
-        }, 2000);
-    };
     render() {
         return (
             <Store.Provider value={this.state}>
